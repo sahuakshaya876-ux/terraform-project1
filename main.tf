887aba0d67f9f7517dd84f3cc6934fa4fa548746
sahuakshaya876-ux <sahuakshaya876@gmail.com>
2026-09-04 14:39:37 +0530

    Added Terraform project

---
+++ main.tf
@@ -0,0 +1,109 @@
+
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  ami             = "ami-0ac7b260cf76d8865"
+  instance_type   = "t3.micro"
+  key_name        = "key1"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1a"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl enable --now httpd
+echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-1"
+  }
+}
+
+resource "aws_instance" "two" {
+  ami             = "ami-0ac7b260cf76d8865"
+  instance_type   = "t3.micro"
+  key_name        = "key1"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1b"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl enable --now httpd
+echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-2"
+  }
+}
+
+resource "aws_instance" "three" {
+  ami             = "ami-0ac7b260cf76d8865"
+  instance_type   = "t3.micro"
+  key_name        = "key1"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "app-server-1"
+  }
+}
+
+resource "aws_instance" "four" {
+  ami             = "ami-0ac7b260cf76d8865"
+  instance_type   = "t3.micro"
+  key_name        = "key1"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "app-server-2"
+  }
+}
+
+resource "aws_security_group" "five" {
+  name = "elb-sg12435895"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "six" {
+  bucket = "terrajenkinsproject12345678934"
+}
+
+resource "aws_iam_user" "seven" {
+for_each = var.user_names
+name = each.value
+}
+
+variable "user_names" {
+description = "*"
+type = set(string)
+default = ["user111", "user222", "user333", "user444"]
+}
+
+resource "aws_ebs_volume" "eight" {
+ availability_zone = "ap-south-1a"
+  size = 40
+  tags = {
+    Name = "ebs-10124"
+  }
+}
